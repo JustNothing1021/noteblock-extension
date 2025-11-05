23bdfc5be4581c0f8d668e39100f504956bad1b3
JustNothing1021 <ljy0909112022@163.com>
2025-11-05 09:05:48 +0800

    Initial commit

---
+++ src/client/java/com/justnothing/noteblock_extension/ClientNoteBlockExtension.java
@@ -0,0 +1,19 @@
+package com.justnothing.noteblock_extension;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Environment(EnvType.CLIENT)
+public class ClientNoteBlockExtension implements ClientModInitializer {
+
+    // public static final Logger LOGGER = LogManager.getLogger("noteblock-extension");
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
